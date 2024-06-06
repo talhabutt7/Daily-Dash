678dd245bb42d09b72a19094752ff0c51ef22e6c
talhabutt7 <talhabuttt@gmail.com>
2024-06-06 09:49:29 +0100

    Scopes added for published, draft and scheduled blog posts

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,7 @@
 class BlogPost < ApplicationRecord
   validates :title, :body, presence: true
+
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.zone.now).order("published_at DESC") }
+  scope :scheduled, -> { where("published_at > ?", Time.zone.now).order("published_at DESC") }
 end
