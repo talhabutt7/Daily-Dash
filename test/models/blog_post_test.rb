e5b8cdcddb0875e632bd8cd80b8c11e804d75012
talhabutt7 <talhabuttt@gmail.com>
2024-06-07 11:46:14 +0100

    Code refactoring for test cases: Helper methods are added not exclude code repetition

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -2,39 +2,51 @@ require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns true for draft blog post" do
-    assert BlogPost.new(published_at: nil).draft?
+    assert new_draft_post.draft?
   end
 
   test "draft? returns false for published blog post" do
-    refute BlogPost.new(published_at: 1.year.ago).draft?
+    refute new_published_post.draft?
   end
 
   test "draft? returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).draft?
+    refute new_scheduled_post.draft?
   end
 
   test "published? returns true for published blog post" do
-    assert BlogPost.new(published_at: 1.year.ago).published?
+    assert new_published_post.published?
   end
 
   test "published? returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).published?
+    refute new_draft_post.published?
   end
 
   test "published? returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).published?
+    refute new_scheduled_post.published?
   end
 
   test "scheduled? returns true for scheduled blog post" do
-    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+    assert new_scheduled_post.scheduled?
   end
 
   test "scheduled? returns false for published blog post" do
-    refute BlogPost.new(published_at: 1.year.ago).scheduled?
+    refute new_published_post.scheduled?
   end
 
   test "scheduled? returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).scheduled?
+    refute new_draft_post.scheduled?
+  end
+
+  def new_draft_post
+    BlogPost.new(published_at: nil)
+  end
+
+  def new_scheduled_post
+    BlogPost.new(published_at: 1.year.from_now)
+  end
+
+  def new_published_post
+    BlogPost.new(published_at: 1.year.ago)
   end
 
 end
