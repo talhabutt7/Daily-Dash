bda5398b508744ec13310ff3aa4256074ff5882d
talhabutt7 <talhabuttt@gmail.com>
2024-06-07 11:32:12 +0100

    published? returns true for published blog post and false for scheduled and draft blog post test cases.

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -13,4 +13,17 @@ class BlogPostTest < ActiveSupport::TestCase
     refute BlogPost.new(published_at: 1.year.from_now).draft?
   end
 
+  test "published? returns true for published blog post" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? returns false for draft blog post" do
+    refute BlogPost.new(published_at: nil).published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).published?
+  end
+
+
 end
