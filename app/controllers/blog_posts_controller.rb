76f0f0ab03d78773acf925a26c81c6edae5a3dad
talhabutt7 <talhabuttt@gmail.com>
2024-06-08 23:59:41 +0100

    COde refactor

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -43,35 +43,6 @@ class BlogPostsController < ApplicationController
   def blog_post_params
     params.require(:blog_post).permit(:title, :content, :published_at)
   end
-
-  def user_signed_in?
-    user_signed_in?
-  end
-
-  def authenticate_user!
-    redirect_to root_path, alert: 'Please sign in.' unless user_signed_in?
-  end
-
-  def set_blog_posts
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-  end
-
-  def set_blog_posts_by_user
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-  end
-
-  def set_blog_posts_by_date
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-  end
-
-  def set_blog_posts_by_title
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-  end
-
-  def set_blog_posts_by_published_at
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-  end
-
   def set_blog_post
     @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
