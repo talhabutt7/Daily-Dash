e39f81374192186c379777446acbdd0358a6a36b
talhabutt7 <talhabuttt@gmail.com>
2024-06-12 14:20:03 +0100

    Navbar javascript added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,14 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+
+// app/javascript/packs/navbar.js
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('button.mobile-menu-button');
+    const menu = document.querySelector('.mobile-menu');
+
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+});
