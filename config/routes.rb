e5d20fec5c207c00a21312494329391651f09671
talhabutt7 <talhabuttt@gmail.com>
2024-06-15 10:01:18 +0100

    Routes updated for Blog posts and likes

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "blog_posts#index"
 
-  resources :blog_posts
+  # resources :blog_posts
   # get "/blog_posts/new", to: "blog_posts#new", as: "new_blog_post"
   # get "/blog_posts/:id", to: "blog_posts#show", as: "blog_post"
   # patch "/blog_posts/:id", to: "blog_posts#update"
   # delete "/blog_posts/:id", to: "blog_posts#destroy"
   # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: "edit_blog_post"
   # post "/blog_posts", to: "blog_posts#create", as: "blog_posts"
+
+  resources :blog_posts do
+    member do
+      post 'like'
+      delete 'unlike'
+    end
+  end
 
 end
