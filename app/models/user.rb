5f7847bb69d42ec58d663ee0e49d161a40de1115
talhabutt7 <talhabuttt@gmail.com>
2024-06-18 20:53:36 +0100

    User Model include the new attributes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,22 @@ class User < ApplicationRecord
 
   has_many :blog_posts, dependent: :destroy
   has_many :likes
+
+  has_one_attached :profile_image
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, :date_of_birth, presence: true
+  validates :terms_and_conditions, acceptance: true
+  validate :date_of_birth_should_be_16_or_older
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+  def date_of_birth_should_be_16_or_older
+    if self.date_of_birth.present? && self.date_of_birth > 16.years.ago.to_date
+      errors.add(:date_of_birth, "must be 16 years or older")
+    end
+  end
 end
