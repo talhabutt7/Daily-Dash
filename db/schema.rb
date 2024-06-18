517e2d12aa0ed66ce9b0dd38ea6f2dc570557582
talhabutt7 <talhabuttt@gmail.com>
2024-06-18 20:52:35 +0100

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_15_085439) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_182716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_15_085439) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.date "date_of_birth"
+    t.boolean "weekly_email"
+    t.boolean "terms_and_conditions"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
