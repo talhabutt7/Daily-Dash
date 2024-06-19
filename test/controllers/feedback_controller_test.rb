1d297c99421891a956a4d075b8c82490cab8af7f
talhabutt7 <talhabuttt@gmail.com>
2024-06-19 11:50:38 +0100

    Feedback files

---
+++ test/controllers/feedback_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class FeedbackControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get feedback_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get feedback_create_url
+    assert_response :success
+  end
+end
