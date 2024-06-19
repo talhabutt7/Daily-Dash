2e7fb5007d52de401217e77491d513310e088ac8
talhabutt7 <talhabuttt@gmail.com>
2024-06-19 10:29:19 +0100

    New validations, Accessors, Callbacks added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,52 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Associations
   has_many :blog_posts, dependent: :destroy
   has_many :likes
 
+  # Image upload
   has_one_attached :profile_image
 
-  validates :username, :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, :date_of_birth, presence: true
-  validates :terms_and_conditions, acceptance: true
-  validate :date_of_birth_should_be_16_or_older
+  # Accessors
+  attr_accessor :accept_terms
 
+  # Validations
+  validates :username, uniqueness: true, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :accept_terms, acceptance: true
+  validates :profile_image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validate :age_must_be_16_or_older
+
+  # Callbacks
+  before_save :downcase_email
+
+
+  # Returns the full name
   def full_name
     "#{first_name} #{last_name}"
   end
 
+  # Returns the age
+  def age
+    if date_of_birth.present?
+      (Date.today - date_of_birth).to_i / 365
+    end
+  end
+
   private
-  def date_of_birth_should_be_16_or_older
-    if self.date_of_birth.present? && self.date_of_birth > 16.years.ago.to_date
-      errors.add(:date_of_birth, "must be 16 years or older")
+
+  # Convert email to all lower-case
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Age must be 16 or older
+  def age_must_be_16_or_older
+    if date_of_birth.present? && date_of_birth > 16.years.ago.to_date
+      errors.add(:date_of_birth, 'You should be 16 years old or older.')
     end
   end
 end
