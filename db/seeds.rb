f84a787dc831bfa81130ecc183c9399de35281ca
talhabutt7 <talhabuttt@gmail.com>
2024-06-19 13:03:14 +0100

    Seed file updated for Admin User

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,23 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.where(email: "tb@tb.com").first_or_initialize
+
+# Create a User
+user = User.where(email: "admin@admin.com").first_or_initialize
 user.update!(
-  password: "password",
-  password_confirmation: "password"
+  email: 'admin@admin.com',
+  password: 'password',
+  first_name: 'Talha',
+  last_name: 'Butt',
+  username: 'admin',
+  date_of_birth: Date.new(1990, 1, 1),
+  weekly_email: true,
+  accept_terms: true
 )
+
+# Attach profile image (if using Active Storage)
+file_path = Rails.root.join('app', 'assets', 'images', 'profile_pic.jpg')
+user.profile_image.attach(io: File.open(file_path), filename: 'profile_pic.jpg', content_type: 'image/jpg')
+
+# Output to console
+puts "User created with email: #{user.email}"
