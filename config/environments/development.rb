1f2bff38d52b130228d4df007eaa782b540e0769
talhabutt7 <talhabuttt@gmail.com>
2024-06-20 17:02:24 +0100

    Enable email deliveries and update SMTP settings for Gmail in the development environment configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Use SMTP for sending emails
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
