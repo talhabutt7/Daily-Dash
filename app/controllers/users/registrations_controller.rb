e2acaeb4c5f47954cdb971edd189b3426dae33ef
talhabutt7 <talhabuttt@gmail.com>
2024-06-21 07:17:57 +0100

    configure_permitted_parameters in Application controller. protect_from_forgery with: :exception ADDED

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,12 +56,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :date_of_birth, :weekly_email, :profile_image])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :date_of_birth, :weekly_email, :accept_terms])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth, :weekly_email, :profile_image])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth])
   end
 
   # The path used after sign up.
